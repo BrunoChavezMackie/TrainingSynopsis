0e838823ec577ee646f36b2ef1798ed6d870d64a
Elvis Perez <elperez@synopsis.ws>
2021-11-04 15:03:36 -0500

    Commit inicial

---
+++ src/main/java/ws/synopsis/trainingdatabase/TrainingdatabaseApplication.java
@@ -0,0 +1,13 @@
+package ws.synopsis.trainingdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingdatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/synopsis/trainingdatabase/TrainingdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.synopsis.trainingdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
